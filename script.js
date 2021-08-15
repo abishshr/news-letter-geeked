c0e5e504e82e0b1f26a39f4073dea4ca9b20b362
Abish Shreedharan <abishshr@gmail.com>
2021-08-15 19:09:01 -0400

    added Header ,Footer,Logo

---
+++ script.js
@@ -0,0 +1,11 @@
+function hamburgerFunc() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") 
+        {
+        x.className += " responsive";
+        } 
+    else 
+        {
+        x.className = "nav";
+        }
+  }
